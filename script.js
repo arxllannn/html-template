c9dc9cc7eb0bc41f9096c904acfc19394d51c3a5
arxllannn <147667804+arxllannn@users.noreply.github.com>
2024-04-22 02:24:42 -0700

    alter

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 
+<<<<<<< Updated upstream
         function openCity(evt, cityName) {
+=======
+
+  
+   
+  
+
+ 
+        function tabbb(evt, tabbo) {
+>>>>>>> Stashed changes
             var i, tabcontent, tablinks;
             tabcontent = document.getElementsByClassName("tabcontent");
             for (i = 0; i < tabcontent.length; i++) {
@@ -9,7 +19,7 @@
             for (i = 0; i < tablinks.length; i++) {
                 tablinks[i].className = tablinks[i].className.replace(" active", "");
             }
-            document.getElementById(cityName).style.display = "block";
+            document.getElementById(tabbo).style.display = "block";
             evt.currentTarget.className += " active";
         }
 
