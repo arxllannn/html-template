f89025220c9a27a66782ea1259abd41779039ad4
Arslan Shahid <147667804+arxllannn@users.noreply.github.com>
2024-04-22 00:21:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+  
+   
+  
+
+ 
+        function openCity(evt, cityName) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            document.getElementById(cityName).style.display = "block";
+            evt.currentTarget.className += " active";
+        }
+
+
+
+function toggleMenu() {
+var x = document.getElementById("myNavbar");
+var icon = document.querySelector(".icon i");
+
+if (x.className.indexOf("responsive") === -1) {
+x.className += " responsive";
+icon.classList.remove("fa-bars");
+icon.classList.add("fa-times");
+} else {
+x.className = x.className.replace(" responsive", "");
+icon.classList.remove("fa-times");
+icon.classList.add("fa-bars");
+}
+}
